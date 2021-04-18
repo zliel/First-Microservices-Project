1d8055c1a662998c18f1a69079e41311755de893
zliel <zpliel@gmail.com>
2021-04-18 00:12:52 -0700

    Implemented a call to the UserReview Service using a WebClient using a passed in user ID and handled the response to return a Flux of Music Reviews (currently, compositions are hard-coded)

--- MusicReviewService/src/main/java/com/example/MusicReviewService/MusicReviewController.java
+++ MusicReviewService/src/main/java/com/example/MusicReviewService/MusicReviewController.java
@@ -4,24 +4,24 @@ import com.example.MusicReviewService.models.Composition;
 import com.example.MusicReviewService.models.MusicReview;
 import com.example.MusicReviewService.models.Review;
 import com.example.MusicReviewService.models.UserReview;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class MusicReviewController {
+
+    @Autowired
+    WebClient.Builder webClientBuilder;
 
     @GetMapping("/api/musicReview/{userId}")
     public Flux<MusicReview> getMusicReview(@PathVariable("userId") String userId) {
         // This will come from a webClient request using the Review.getCompositionId() method (will be inside the return statement
         Composition testComposition = new Composition("test1", "Nocturne No. 0", "Choppin");
-
-        // This will be retrieved before the Composition, and will be retrieved using the UserReview.getReviews() method
-        Flux<Review> testReviewFlux = Flux.just(new Review(testComposition.getCompositionId(), "It sucked", 4));
-
-        // This will be retrieved first using a webClient request with the user's ID
-        UserReview testReview = new UserReview(userId, testReviewFlux);
 
         // ORDER:
         //      UserReview
@@ -30,8 +30,18 @@ public class MusicReviewController {
         //          return new MusicReview()
         //      );
 
-        return Flux.from(testReview.getReviews()).map(review -> {
-            // In the actual program, we'll get the Composition necessary by making a request using the "review.getCompositionId()" method
+        // Make a WebClient request to the UserReviewService using the given userId and retrieve a Mono containing the user's reviews
+        Mono<UserReview> userReviewMono = webClientBuilder.build().get().uri("http://localhost:8083/api/userReview/" + userId)
+                                         .retrieve()
+                                         .bodyToMono(UserReview.class);
+
+        // Retrieve the list of the user's reviews from the userReviewMono and store its items in a flux
+        Flux<Review> reviews = userReviewMono.flatMapIterable(UserReview::getReviews);
+
+        // map over our list, creating new MusicReview instances from each review and storing them inside a Flux, which we return
+        return reviews.map(review -> {
+            System.out.println(review);
+
             return new MusicReview(testComposition.getName(), testComposition.getComposer(), review.getReview(), review.getRating());
         });
     }
