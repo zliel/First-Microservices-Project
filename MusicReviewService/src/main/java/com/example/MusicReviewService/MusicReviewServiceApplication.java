6bf735187965d527dd525be26ad99f66cb613981
zliel <zpliel@gmail.com>
2021-04-18 00:04:50 -0700

    Added a WebClient Bean and Autowired it in the MusicReviewController class

--- MusicReviewService/src/main/java/com/example/MusicReviewService/MusicReviewServiceApplication.java
+++ MusicReviewService/src/main/java/com/example/MusicReviewService/MusicReviewServiceApplication.java
@@ -2,9 +2,16 @@ package com.example.MusicReviewService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MusicReviewServiceApplication {
+
+	@Bean
+	public WebClient getWebClient() {
+		return WebClient.create();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicReviewServiceApplication.class, args);
