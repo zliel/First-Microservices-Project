0372eb342eb4ff626cb6eecf5693d76153d3d75f
zliel <zpliel@gmail.com>
2021-04-18 17:42:45 -0700

    Changed the "MusicPieceService" to the CompositionService (they'll have the same function, but composition is a more useful name)

---
+++ CompositionService/src/main/java/com/example/CompositionService/CompositionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.CompositionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositionServiceApplication.class, args);
+	}
+
+}
---
+++ CompositionService/src/test/java/com/example/CompositionService/CompositionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CompositionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
