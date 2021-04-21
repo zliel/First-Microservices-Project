4a725103d4471fabfab5a9c494e0f57afaa012ea
zliel <zpliel@gmail.com>
2021-04-20 22:23:09 -0700

    Added @EnableEurekaClient annotations to let the Discovery Server know where to find each service

--- MusicReviewService/src/main/java/com/example/MusicReviewService/MusicReviewServiceApplication.java
+++ MusicReviewService/src/main/java/com/example/MusicReviewService/MusicReviewServiceApplication.java
@@ -2,13 +2,17 @@ package com.example.MusicReviewService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MusicReviewServiceApplication {
 
 	@Bean
+	@LoadBalanced
 	public WebClient.Builder getWebClientBuilder() {
 		return WebClient.builder();
 	}
--- UserReviewService/src/main/java/com/example/UserReviewService/UserReviewServiceApplication.java
+++ UserReviewService/src/main/java/com/example/UserReviewService/UserReviewServiceApplication.java
@@ -2,9 +2,20 @@ package com.example.UserReviewService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserReviewServiceApplication {
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserReviewServiceApplication.class, args);
